bd6d400c43fb6d74d673bb7f1c98090214877bc3
Eric Andren <ericrandren@gmail.com>
2018-08-28 21:38:33 -0700

    First big one

--- footer.php
+++ footer.php
@@ -4,126 +4,86 @@
 					</div>
 					<section class="flex">
 						<div class="column">
-							<h5>Main</h5>
-							<ul class="links">
-								<li><a href="<?php echo site_url('/work/', 'http'); ?>">Work</a></li>
-								<li><a href="<?php echo site_url('/about/', 'http'); ?>">About ozpoø</a></li>
-								<li><a href="<?php echo site_url('/news/', 'http'); ?>">News</a></li>
-							</ul>
-							<ul class="links">
-								<li><a href="<?php echo site_url('/essay/', 'http'); ?>">Essays</a></li>
-								<li><a href="<?php echo site_url('/360/', 'http'); ?>">360 Views</a></li>
-								<li><a href="<?php echo site_url('/glossary/', 'http'); ?>">Glossary</a></li>
-								<li><a href="<?php echo site_url('/library/', 'http'); ?>">Library</a></li>
-								<li><a href="<?php echo site_url('/resources/', 'http'); ?>">Resources</a></li>
+							<h5 class="cap">Menu</h5>
+							<ul>
+								<li><a href="<?php echo site_url('/', 'http'); ?>">Home</a></li>
+								<li><a href="<?php echo site_url('/about/', 'http'); ?>">About</a></li>
+								<li><a href="<?php echo site_url('/contact/', 'http'); ?>">Contact</a></li>
 							</ul>
 						</div>
 						<div class="column">
-							<h5>Connect</h5>
-							<ul class="links">
-								<li><a href="mailto:hello@ozpoo.co">hello@ozpoo.co</a></li>
-							</ul>
-							<ul class="links">
-								<li><a href="https://www.facebook.com/ozpoo/" target="_blank">Facebook</a></li>
-								<li><a href="https://twitter.com/ozpoo/" target="_blank">Twitter</a></li>
-								<li><a href="https://www.instagram.com/ozpoo/" target="_blank">Instagram</a></li>
-								<li><a href="https://www.linkedin.com/ozpoo/" target="_blank">LinkedIn</a></li>
-							</ul>
-							<h5>Academia</h5>
-							<ul class="links">
-								<li><a href="<?php echo site_url('/research/', 'http'); ?>">Research</a></li>
-								<li><a href="<?php echo site_url('/teaching-philosophy/', 'http'); ?>">Teaching Philosophy</a></li>
-								<li><a href="<?php echo site_url('/design-philosophy/', 'http'); ?>">Design Philosophy</a></li>
-								<li><a href="<?php echo site_url('/student-work/', 'http'); ?>">Student Work</a></li>
-							</ul>
-							<!-- <ul class="links">
-								<li><a href="<?php echo site_url('/work/', 'http'); ?>">All Work</a></li>
-							</ul>
-							<ul class="links">
-								<?php
-									$terms = get_terms([
-								    'taxonomy' => "work_category",
-								    'hide_empty' => true,
-									]);
-									foreach ($terms as $term) {
-								?>
-								<?php $url = "/work/category/" . $term->slug; ?>
-									<li><a href="<?php echo site_url($url, 'http'); ?>"><?php echo $term->name; ?></a></li>
-								<?php
-									}
-								?>
-							</ul> -->
+							<h5 class="cap">Contact</h5>
+							<p>
+								Barbara Glass, Life Strategist
+								<a href="tel:+1-206-839-7474">+1 206 839 7474</a>
+								<a href="mailto:barbara@evokeseattle.com">barbara@evokeseattle.com</a><br>
+								@evokeseattle
+							</p>
+							<h5 class="cap">Located At</h5>
+							<p>
+								5400 Carillon Point, Building 5000, 4th Floor<br>
+								Kirkland, WA 98033
+							</p>
+							<p><a href="https://goo.gl/maps/ULYRa1xR9sA2" target="_blank">Get Directions</a>
+							<h5 class="cap">Rates</h5>
+							<p>
+								$105 per 1 hour session<br>
+								Extended sessions are prorated on a $105/hr basis.
+							</p>
 						</div>
 					</section>
 				</nav>
 
 				<footer>
 					<section class="flex">
-						<section class="column">
-							<h4>Menu</h4>
-							<ul class="links">
-								<li><a href="<?php echo site_url('/work/', 'http'); ?>">Work</a></li>
-								<li><a href="<?php echo site_url('/about/', 'http'); ?>">About ozpoø</a></li>
-								<li><a href="<?php echo site_url('/news/', 'http'); ?>">News</a></li>
-							</ul>
-							<ul class="links">
-								<li><a href="<?php echo site_url('/essay/', 'http'); ?>">Essays</a></li>
-								<li><a href="<?php echo site_url('/360/', 'http'); ?>">360 Views</a></li>
-								<li><a href="<?php echo site_url('/glossary/', 'http'); ?>">Glossary</a></li>
-								<li><a href="<?php echo site_url('/library/', 'http'); ?>">Library</a></li>
-								<li><a href="<?php echo site_url('/resources/', 'http'); ?>">Resources</a></li>
-							</ul>
+						<section class="contact-footer">
+							<h5 class="cap gold">Contact</h5>
+							<p>
+								Barbara Glass, Life Strategist<br>
+								<a href="tel:+1-206-839-7474">+1 206 839 7474</a><br>
+								<a href="mailto:barbara@evokeseattle.com">barbara@evokeseattle.com</a><br>
+								@evokeseattle
+							</p>
 						</section>
-						<section class="column">
-							<h4>Academia</h4>
-							<ul class="links">
-								<li><a href="<?php echo site_url('/research/', 'http'); ?>">Research</a></li>
-								<li><a href="<?php echo site_url('/teaching-philosophy/', 'http'); ?>">Teaching Philosophy</a></li>
-								<li><a href="<?php echo site_url('/design-philosophy/', 'http'); ?>">Design Philosophy</a></li>
-								<li><a href="<?php echo site_url('/student-work/', 'http'); ?>">Student Work</a></li>
-							</ul>
+						<section class="located-at-footer">
+							<h5 class="cap gold">Located At</h5>
+							<p>
+								5400 Carillon Point, Building 5000, 4th Floor<br>
+								Kirkland, WA 98033
+							</p>
+							<p><a href="https://goo.gl/maps/ULYRa1xR9sA2" target="_blank">Get Directions</a>
 						</section>
-						<section class="column">
-							<h4>Work</h4>
-							<ul class="links">
-								<li><a href="<?php echo site_url('/work/', 'http'); ?>">All Work</a></li>
-							</ul>
-							<ul class="links">
-								<?php
-									$terms = get_terms([
-								    'taxonomy' => "work_category",
-								    'hide_empty' => true,
-									]);
-									foreach ($terms as $term) {
-								?>
-								<?php $url = "/work/category/" . $term->slug; ?>
-									<li><a href="<?php echo site_url($url, 'http'); ?>"><?php echo $term->name; ?></a></li>
-								<?php
-									}
-								?>
-							</ul>
-						</section>
-						<section class="column">
-							<h4>Contact</h4>
-							<ul class="links">
-								<li><a href="mailto:hello@ozpoo.co">hello@ozpoo.co</a></li>
-							</ul>
-							<ul class="links">
-								<li><a href="https://www.facebook.com/ozpoo/" target="_blank">Facebook</a></li>
-								<li><a href="https://twitter.com/ozpoo/" target="_blank">Twitter</a></li>
-								<li><a href="https://www.instagram.com/ozpoo/" target="_blank">Instagram</a></li>
-								<li><a href="https://www.linkedin.com/ozpoo/" target="_blank">LinkedIn</a></li>
-							</ul>
+						<section class="rates-footer">
+							<h5 class="cap gold">Rates</h5>
+							<p>
+								$105 per 1 hour session<br>
+								Extended sessions are prorated on a $105/hr basis.
+							</p>
 						</section>
 					</section>
-					<section class="copy">
-						<div class="column left">
-							<p>This site is protected by copyright. &copy;<?php the_date("Y"); ?> Eric 'Oz' Andren.</p>
-						</div>
-						<div class="column right">
-							<p><a href="<?php echo site_url('/privacy-policy/', 'http'); ?>">Privacy Policy</a></p>
-						</div>
-					</section>
+					<div class="logo">
+						<p>
+							<a href="<?php echo site_url('/', 'http'); ?>">
+								<svg version="1.1"
+									 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
+									 x="0px" y="0px" width="150px" height="39.7px" viewBox="0 0 150 39.7" style="enable-background:new 0 0 150 39.7;"
+									 xml:space="preserve">
+								<polygon class="st0" points="57,10.7 45.1,35.2 45,35.2 33,10.7 29.5,10.7 43.5,38.9 46.5,38.9 60.5,10.7 	"/>
+								<path class="st0" d="M14.9,9.9C6.7,9.9,0,16.5,0,24.8C0,33,6.7,39.7,14.9,39.7c5.3,0,9.9-2.8,12.6-6.9h-4c-2.1,2.3-5.2,3.7-8.5,3.7
+									C9,36.5,4.1,32.1,3.3,26.4l26.4,0c0.1-0.5,0.1-1.1,0.1-1.6C29.8,16.5,23.2,9.9,14.9,9.9z M3.3,23.2C4.1,17.5,9,13.1,14.9,13.1
+									c5.9,0,10.8,4.4,11.6,10.1H3.3z"/>
+								<path class="st0" d="M150,24.8c0-8.2-6.7-14.9-14.9-14.9s-14.9,6.7-14.9,14.9c0,8.2,6.7,14.9,14.9,14.9c5.3,0,9.9-2.8,12.6-6.9h-4
+									c-2.1,2.3-5.2,3.7-8.5,3.7c-5.9,0-10.8-4.4-11.6-10.1l26.4,0C150,25.8,150,25.3,150,24.8z M123.5,23.2c0.8-5.7,5.7-10.1,11.6-10.1
+									s10.8,4.4,11.6,10.1H123.5z"/>
+								<polygon class="st0" points="120.3,10.7 115.3,10.7 98.3,25 98.2,25 98.2,0 95,0 95,38.9 98.2,38.9 98.2,29.2 104.2,24.1
+									104.3,24.1 116.7,38.9 121,38.9 106.7,22.1 	"/>
+								<path class="st0" d="M75.1,9.9c-8.2,0-14.9,6.7-14.9,14.9c0,8.2,6.7,14.9,14.9,14.9c8.2,0,14.9-6.7,14.9-14.9
+									C90.1,16.5,83.4,9.9,75.1,9.9z M75.1,36.5c-6.5,0-11.7-5.3-11.7-11.7s5.3-11.7,11.7-11.7c6.5,0,11.7,5.3,11.7,11.7
+									S81.6,36.5,75.1,36.5z"/>
+								</svg>
+							</a>
+						</p>
+					</div>
 				</footer>
 			</div>
 		</section>
