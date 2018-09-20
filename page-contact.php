83f449216a97c2e10a95005b180859a8609770a2
Eric Andren <ericrandren@gmail.com>
2018-09-20 12:23:09 -0700

    Stuff

--- page-contact.php
+++ page-contact.php
@@ -7,14 +7,14 @@
 			</section>
 
 			<section class="form flex" data-aos="fade-up" data-aos-offset="0" data-aos-once="true" data-aos-easing="ease" data-aos-duration="1200" data-aos-delay="800">
-				<div class="left">
-					<?php echo do_shortcode('[contact-form-7 id="90" title="Contact Form"]'); ?>
-				</div>
 				<div class="right">
 					<h2 class="gold cap" data-aos="fade-up" data-aos-offset="0" data-aos-once="true" data-aos-easing="ease" data-aos-duration="1200" data-aos-delay="0">Don't be shy</h2>
 					<div class="callout">
 						<p><?php echo get_field("call_out"); ?></p>
 					</div>
+				</div>
+				<div class="left">
+					<?php echo do_shortcode('[contact-form-7 id="90" title="Contact Form"]'); ?>
 				</div>
 			</section>
 
